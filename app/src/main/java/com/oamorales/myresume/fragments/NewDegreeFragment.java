70a6ed669d7ac2256827597f02104d402e0556a6
oamorales <68397871+oamorales@users.noreply.github.com>
2020-08-05 22:30:13 -0400

    NewDegreeFragment modifications
    Manifest: se agregó permisos para uso de cámara y configuración de FileProvider.
    NewDegreeFragment: se agregó snakbars, función de cámara para tomar y guardar imágenes, listas para las fechas, iconos para mostrar error por campos requeridos, manejo de permisos para cámara y almacenamiento.
    Se añadieron dos archivos xml: uno para formas circulares y otro para el FileManager.
    Se modificó la vista del fragment new degree.

--- app/src/main/java/com/oamorales/myresume/fragments/NewDegreeFragment.java
+++ app/src/main/java/com/oamorales/myresume/fragments/NewDegreeFragment.java
@@ -1,37 +1,53 @@
 package com.oamorales.myresume.fragments;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 
-import androidx.activity.OnBackPressedCallback;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatImageView;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.provider.Settings;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
-import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.button.MaterialButton;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputEditText;
 import com.oamorales.myresume.R;
 import com.oamorales.myresume.models.Degree;
+import com.squareup.picasso.Picasso;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Objects;
 
 import io.realm.Realm;
-import io.realm.RealmConfiguration;
 
 public class NewDegreeFragment extends Fragment implements View.OnClickListener, TextView.OnEditorActionListener{
 
@@ -43,9 +59,16 @@ public class NewDegreeFragment extends Fragment implements View.OnClickListener,
     private AppCompatImageView newDegreeLogo;
     private int yearB, yearE;
 
-    private List<Integer> years = new ArrayList<>();
-    private final int DEFAULT_VALUE = -8;
+    private final String MEDIA_DIRECTORY = "curriculumFiles/" + "media";
+    private final String TEMPORAL_PICTURE_NAME = "temporal.jpg";
+    private final int GALLERY_REQUEST_CODE = 1000;
+    private final int CAMERA_REQUEST_CODE = 1001;
 
+    private List<Integer> years = new ArrayList<>();
+    private final int WRITE_EXTERNAL_STORAGE_REQUEST_CODE = 1000;
+
+    private String absolutePath;
+    private Uri imageUri;
 
     public NewDegreeFragment() {
         // Required empty public constructor
@@ -67,7 +90,7 @@ public class NewDegreeFragment extends Fragment implements View.OnClickListener,
             years.add(year);
             year--;
         }
-        ArrayAdapter<Integer> adapter = new ArrayAdapter<Integer>(getContext(), R.layout.degrees_list_years, years);
+        ArrayAdapter<Integer> adapter = new ArrayAdapter<Integer>(requireContext(), R.layout.degrees_list_years, years);
         newDegreeYearBegin.setAdapter(adapter);
         newDegreeYearEnd.setAdapter(adapter);
         return view;
@@ -93,13 +116,17 @@ public class NewDegreeFragment extends Fragment implements View.OnClickListener,
         newDegreeYearEnd.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                view.getId();
                 newDegreeYearEnd.setError(null);
                 yearE = years.get(position);
             }
         });
 
-
+        newDegreeLogo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMediaStore();
+            }
+        });
     }
 
     @Override
@@ -128,11 +155,11 @@ public class NewDegreeFragment extends Fragment implements View.OnClickListener,
         String discipline = Objects.requireNonNull(newDegreeDiscipline.getText()).toString();
         String gradeAverage = Objects.requireNonNull(newDegreeAverage.getText()).toString();
 
-
         if (!tittle.isEmpty() && !university.isEmpty() && !discipline.isEmpty()
                 && !gradeAverage.isEmpty() && yearB!=0 && yearE!=0){
             if (yearE >= yearB){
-                Degree newDegree = new Degree(R.drawable.usb_logo,tittle, university, discipline, yearB, 2019, Double.parseDouble(gradeAverage));
+
+                Degree newDegree = new Degree(R.drawable.usb_logo,tittle, university, discipline, yearB, yearE, Double.parseDouble(gradeAverage));
                 realm.beginTransaction();
                 realm.copyToRealmOrUpdate(newDegree);
                 realm.commitTransaction();
@@ -150,10 +177,7 @@ public class NewDegreeFragment extends Fragment implements View.OnClickListener,
             newDegreeAverage.setError(gradeAverage.isEmpty() ? "required" : null);
             newDegreeYearBegin.setError((yearB == 0) ? "required" : null);
             newDegreeYearEnd.setError((yearE == 0) ? "required" : null);
-
         }
-
-
     }
 
     /** Eliminar icono de error al escribir en los EditText */
@@ -163,5 +187,190 @@ public class NewDegreeFragment extends Fragment implements View.OnClickListener,
         return true;
     }
 
+    /** newDegreeLogo Click event */
+    private void openMediaStore(){
+        if (!isExternalStorageWritable()){
+            Snackbar.make(newDegreeLogo, "Memory Access Denied", Snackbar.LENGTH_LONG).setAnimationMode(Snackbar.ANIMATION_MODE_SLIDE).show();
+        }else{
+            if (haveStoragePermission()){
+                pickImage();
+            }else{
+                requestPermission();
+            }
+        }
+    }
 
+    /** Checks if external storage is available for read and write */
+    public boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        return Environment.MEDIA_MOUNTED.equals(state);
+    }
+
+    /** Checks if external storage is available to at least read */
+    public boolean isExternalStorageReadable() {
+        String state = Environment.getExternalStorageState();
+        return (Environment.MEDIA_MOUNTED.equals(state) ||
+                Environment.MEDIA_MOUNTED_READ_ONLY.equals(state));
+    }
+
+    /** Check if permission granted */
+    private boolean haveStoragePermission(){
+        return ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    /** Request permission */
+    private void requestPermission(){
+        String[] permissions = {Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA};
+        requestPermissions(permissions, WRITE_EXTERNAL_STORAGE_REQUEST_CODE);
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case WRITE_EXTERNAL_STORAGE_REQUEST_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    pickImage();
+                }else {
+                    boolean showRationale = shouldShowRequestPermissionRationale(Manifest.permission.READ_EXTERNAL_STORAGE);
+                    if (showRationale){
+                        Snackbar.make(newDegreeLogo,"Debe habilitar permiso al almacenamiento para poder cargar el logo", Snackbar.LENGTH_LONG)
+                                .show();
+                    }else {
+                        AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
+                        builder.setTitle("HABILITAR PERMISO")
+                                .setMessage("Para poder cargar el logo es necesario que habilite el permiso de acceso al almacenamiento.\n" +
+                                "¿Desea habilitar el permiso mencionado?").setPositiveButton("Sí", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                goToSettings();
+                            }
+                        }).setNegativeButton("No",null)
+                        .show();
+                    }
+                }
+        }
+    }
+
+    /** Displays this app settings menu */
+    private void goToSettings(){
+        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS, Uri.parse("package:com.oamorales.mycurriculum"));
+        intent.addCategory(Intent.CATEGORY_DEFAULT).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
+    /** Pick image logo */
+    private void pickImage(){
+        final String gallery = "Galería", camera = "Cámara", cancel = "Cancel";
+        final CharSequence[] items = {gallery, camera, cancel};
+        AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
+        builder.setTitle("Seleccione Opción")
+                .setItems(items, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if (items[which].equals(gallery)){
+                            selectFromGallery();
+                        }else if (items[which].equals(camera)){
+                            takePicture();
+                        }else{
+                            dialog.dismiss();
+                        }
+                    }
+                }).show();
+    }
+
+    private void selectFromGallery(){
+        Intent in = new Intent(Intent.ACTION_PICK);
+        in.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,"image/*");
+        startActivityForResult(Intent.createChooser(in, "Seleccione aplicación"), GALLERY_REQUEST_CODE);
+    }
+
+
+    private void takePicture() {
+        //SE CREA LA CARPETA PARA ALMACENAR LAS IMÁGENES
+        String mainPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)+ "/curriculum_images";
+        long suffix = System.currentTimeMillis()/1000;
+        absolutePath = mainPath + File.separator + "img_" + suffix + ".png";
+        File newFolder = new File(mainPath);
+        if (!newFolder.exists()){
+            newFolder.mkdir();
+        }
+        File imageFile = new File(absolutePath);
+        imageUri = FileProvider.getUriForFile(requireContext(), "com.oamorales.myresume.provider", imageFile);
+
+
+        /*SimpleDateFormat sdf =  (SimpleDateFormat) SimpleDateFormat.getDateTimeInstance();
+        SimpleDateFormat format = (SimpleDateFormat) SimpleDateFormat.getDateTimeInstance();
+        String dt = sdf.format(new Date());*/
+
+        //RUTA DONDE SE ALMACENA LA IMAGEN TOMADA
+        //File path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        //absolutePath = path.getAbsolutePath() + "/curriculum_images/" + "img_prueba_"+ dt +".png";
+        //File imageFile = new File(path.getAbsolutePath() + "/curriculum_images", "img_prueba_"+ dt +".png");
+        //Uri imageUri = FileProvider.getUriForFile(requireContext(), "com.oamorales.myresume.provider", imageFile);
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        Intent in = new Intent(Intent.ACTION_GET_CONTENT);
+
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+        if (intent.resolveActivity(requireActivity().getPackageManager()) != null)
+            startActivityForResult(intent, CAMERA_REQUEST_CODE);
+        else
+            Snackbar.make(newDegreeLogo,"Imposible tomar foto", Snackbar.LENGTH_SHORT).show();
+
+        /*Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (intent.resolveActivity(requireActivity().getPackageManager()) != null)
+            startActivityForResult(intent, CAMERA_REQUEST_CODE);
+        else
+            Snackbar.make(newDegreeLogo,"Imposible tomar foto", Snackbar.LENGTH_SHORT).show();*/
+    }
+
+    /** Executed after select or capture image */
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        switch (requestCode){
+            case GALLERY_REQUEST_CODE:
+                if (resultCode== AppCompatActivity.RESULT_OK){
+                    assert data != null;
+                    //Picasso.get().load(data.getData()).fit().into(newDegreeLogo);
+                    Picasso.get().load(data.getDataString()).fit().into(newDegreeLogo);
+                }else{
+                    Snackbar.make(newDegreeLogo, "FALLO AL OBTENER IMAGEN", Snackbar.LENGTH_SHORT);
+                }
+                return;
+            case CAMERA_REQUEST_CODE:
+                if (resultCode== AppCompatActivity.RESULT_OK){
+                    Picasso.get().load(imageUri).fit().into(newDegreeLogo);
+                }else{
+                    Snackbar.make(newDegreeLogo,"Error al tomar foto",Snackbar.LENGTH_SHORT);
+                }
+            default:
+                super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    private void decodeBitmap(Bundle bundle) {
+        Bitmap imageBitmap = (Bitmap) bundle.get("data");
+        newDegreeLogo.setBackground(null);
+        newDegreeLogo.setImageBitmap(imageBitmap);
+    }
+
+    private void loadImage(){
+        //File newPicture = new File(Environment.getExternalStorageDirectory().getPath() + "image1" + ".png");
+        //MEDIA_DIRECTORY = "curriculumFiles/" + "media";
+        // TEMPORAL_PICTURE_NAME = "temporal.jpg";
+
+        File imgFile = new File(Environment.getExternalStorageDirectory() + MEDIA_DIRECTORY);
+        /*File file = new File(Environment.getExternalStoragePublicDirectory(), MEDIA_DIRECTORY);
+        file.mkdirs();*/
+        //dirección temporal donde se almacena la foto tomada
+        String path = Environment.getExternalStorageDirectory() + File.separator + MEDIA_DIRECTORY
+                + File.separator + TEMPORAL_PICTURE_NAME;
+
+        File newFile = new File(path);
+
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(newFile));
+        startActivityForResult(intent, CAMERA_REQUEST_CODE);
+    }
 }
